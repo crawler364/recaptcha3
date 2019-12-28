d2fb8deb55932ab036b0dcc50b09f5100e343367
darkside <crawler364@gmail.com>
2019-12-28 13:15:23 +0300

    recaptcha 0.2 static

--- local/components/wc/recaptcha3/templates/.default/script.js
+++ local/components/wc/recaptcha3/templates/.default/script.js
@@ -3,55 +3,52 @@ class ReCaptcha3 {
         this.props = props;
     }
 
-    getSiteKey() {
-        BX.ajax.runComponentAction('wc:recaptcha3', 'getParams', {
+    static async getParams(props) {
+        let response = await BX.ajax.runComponentAction('wc:recaptcha3', 'getParams', {
             mode: 'class',
-            signedParameters: this.props.signedParameters
-        }).then(function (response) {
-            if (response.status == 'success') {
-                let siteKey = response.data.siteKey;
-                let secretKey = response.data.secretKey;
-                let action = response.data.action;
-                if (typeof (siteKey) != 'undefined' && typeof (secretKey) != 'undefined' && typeof (action) != 'undefined') {
-                    ReCaptcha3.siteVerify(siteKey, secretKey, action);
-                }
+            signedParameters: props.signedParameters
+        });
+        if (response.status == 'success') {
+            let siteKey = response.data.siteKey;
+            let secretKey = response.data.secretKey;
+            let action = response.data.action;
+            if (typeof siteKey != 'undefined' && typeof secretKey != 'undefined' && typeof action != 'undefined') {
+                return response;
+            }
+        }
+    }
+
+    handler(props) {
+        grecaptcha.ready(async function () {
+            let params = await ReCaptcha3.getParams(props);
+            console.log(params);
+            let token = await grecaptcha.execute(params.data.siteKey, {action: params.data.action});
+            let siteVerify = await ReCaptcha3.siteVerify(params.data.secretKey, token);
+            if (siteVerify.data.response.success == true) {
+                let catpchaSid = document.getElementById('wcCaptchaSid').value;
+                document.getElementById('wcCaptchaWord').value = await ReCaptcha3.getCaptchaWord(catpchaSid);
             }
         });
     }
 
-    static siteVerify(siteKey, secretKey, action) {
-        grecaptcha.ready(function () {
-            grecaptcha.execute(siteKey, {action: action}).then(function (token) {
-                BX.ajax.runComponentAction('wc:recaptcha3', 'siteVerify', {
-                    mode: 'class',
-                    data: {
-                        secretKey: secretKey,
-                        token: token,
-                    }
-                }).then(function (response) {
-                    if (response.data.response.success == true) {
-                        let catpchaSid = document.getElementById('wcCaptchaSid').value;
-                        ReCaptcha3.getCaptchaWord(catpchaSid).then(function (captchaWord) {
-                            ReCaptcha3.setCaptchaWord(captchaWord);
-                        });
-                    }
-                });
-            });
-        })
+    static async siteVerify(secretKey, token) {
+        let response = await BX.ajax.runComponentAction('wc:recaptcha3', 'siteVerify', {
+            mode: 'class',
+            data: {
+                secretKey: secretKey,
+                token: token,
+            }
+        });
+        return response;
     }
 
-    static setCaptchaWord(captchaWord) {
-        document.getElementById('wcCaptchaWord').value = captchaWord;
-    }
-
-    static getCaptchaWord(catpchaSid) {
-        return BX.ajax.runComponentAction('wc:recaptcha3', 'getCaptchaWord', {
+    static async getCaptchaWord(catpchaSid) {
+        let responce = await BX.ajax.runComponentAction('wc:recaptcha3', 'getCaptchaWord', {
             mode: 'class',
             data: {
                 catpchaSid: catpchaSid,
             }
-        }).then(function (response) {
-            return response.data.captchaWord;
         });
+        return responce.data.captchaWord;
     }
 }
